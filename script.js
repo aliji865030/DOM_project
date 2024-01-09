9f4a12ea4e94f45c9996d06dbdd9568a2f68e109
aliji865030 <82137372+aliji865030@users.noreply.github.com>
2024-01-09 10:34:47 +0530

    files added

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let contener=document.getElementById("container");
+let content=document.getElementById("pera");
+
+contener.style.backgroundColor="yellow";
+content.style.margin="20px";
+content.style.padding="10px";
+content.style.fontSize="18px";
+content.style.fontWeight="bold";
+contener.style.height="200px";
+contener.style.width="300px";
+content.style.color="blue";
